f2c1a62edd603e23cd496b2a3e11bbf589738041
Rheryson <rherysonlima@gmail.com>
2022-12-24 23:30:35 -0300

    estrutura-repetição

---
+++ controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,20 @@
+public class CaixaEletronico {
+
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 26.0;
+        
+        if(valorSolicitado <= saldo) {
+
+            saldo = saldo -  valorSolicitado;
+            System.out.println("Novo saldo: " + saldo);
+
+        }
+
+            else
+            System.out.println("Saldo insuficiente");
+            
+        System.out.println("Seu saldo atual é " +  saldo);
+    }
+    
+}
---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,54 @@
+public class PlanoOperadora {
+
+    public static void main(String[] args) {
+
+
+    /* 
+        String plano ="M";
+
+        if(plano == "B") {
+
+            System.out.println("100 minutos de ligação");
+
+        }
+
+        else if(plano == "M") {
+
+            System.out.println("100 minutos de ligação");
+            System.out.println("WhatsApp e Instagram grátis");
+        }
+
+        else if(plano == "T") {
+
+            System.out.println("100 minutos de ligação");
+            System.out.println("WhatsApp  e Instagram grátis");
+            System.out.println("5GB Youtube");
+        }
+
+    */
+
+        String plano = "M";
+
+        switch (plano) {
+            
+            case "T": {
+
+                System.out.println("5G Youtube");
+                break;
+            }
+
+            case "M": {
+
+                System.out.println("WhatsApp e Instagram grátis");
+                break;
+            }
+
+            case "B": {
+
+                System.out.println("100 minutos de ligação");
+                break;
+            }
+        }
+    
+    }
+}
